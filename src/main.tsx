c61bf23e75b87e5dd4d5492deae9842aa4fc35b0
Jenova Marie <jenova@recoverysky.org>
2026-03-11 21:43:52 -0500

    ✨ feat(web): integrate Auth0 for portal authentication
    Wrap app in Auth0Provider for SPA authentication flow:
    
    - Add @auth0/auth0-react dependency
    - Configure Auth0Provider in main.tsx with meetingmaker tenant
    - Portal nav button triggers loginWithRedirect()
    - Authenticated users see name + logout button in header
    
    Authored-By: Jenova Marie <jenova-marie@pm.me>

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+      domain="meetingmaker.us.auth0.com"
+      clientId="yzfwOaU5V5wlnSKGsUkUk6mz7IidXaJl"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </StrictMode>
 );
